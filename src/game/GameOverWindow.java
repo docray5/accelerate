d3626aeb099a58c5e5e570b3969114151bf8ba1d
Janek <stonccbonk@gmail.com>
2023-04-01 15:10:59 +0100

    Number of points is now shown and buttons are fixed, so they work only when the window is fully up

--- src/game/GameOverWindow.java
+++ src/game/GameOverWindow.java
@@ -15,25 +15,31 @@ public class GameOverWindow {
 
     private final Pane gameOverPane;
     private final Button restartBtn;
+    private final Label pointsLabel;
+    private final Label overLabel;
 
     public GameOverWindow(Game game) {
         this.gameOverPane = new Pane();
         this.gameOverPane.setVisible(false);
-        this.gameOverPane.setMaxSize(Game.WIDTH/3.0, Game.HEIGHT/3.0);
+        this.gameOverPane.setMaxSize(Game.WIDTH/2.0, Game.HEIGHT/2.5);
         this.gameOverPane.getStyleClass().add("over-pane");
         this.gameOverPane.setTranslateY(350);
 
-        Label overText = new Label("Not gut...");
-        overText.setTextFill(Color.AZURE);
-        overText.setFont(Font.font("Helvetica", 44));
+        this.overLabel = new Label("Damn u ded.");
+        this.overLabel.setTextFill(Color.AZURE);
+        this.overLabel.setFont(Font.font("Helvetica", 44));
+
+        this.pointsLabel = new Label("You got: ");
+        this.pointsLabel.setTextFill(Color.AZURE);
+        this.pointsLabel.setFont(Font.font("Helvetica", 44));
 
         this.restartBtn = new Button("Start over.");
         this.restartBtn.setOnAction(event -> game.restart());
         this.restartBtn.setPrefSize(200, 40);
         this.restartBtn.getStyleClass().add("btn-restart");
 
-        VBox vBox = new VBox(overText, this.restartBtn);
-        vBox.setPrefSize(Game.WIDTH/3.0, Game.HEIGHT/3.0);
+        VBox vBox = new VBox(this.overLabel, pointsLabel, this.restartBtn);
+        vBox.setPrefSize(Game.WIDTH/2.0, Game.HEIGHT/2.5);
         vBox.setPadding(new Insets(10));
         vBox.setSpacing(10);
         vBox.setAlignment(Pos.CENTER);
@@ -45,15 +51,20 @@ public class GameOverWindow {
         return gameOverPane;
     }
 
-    public void display() {
+    public void display(int pointsNum) {
+        if (pointsNum > 49) this.overLabel.setText("Not gut...");
+        if (pointsNum > 99) this.overLabel.setText("RAGE QUIT!!!");
+        this.pointsLabel.setText("You got: " + pointsNum + " points.");
         this.gameOverPane.setVisible(true);
+        this.restartBtn.setDisable(false);
         TranslateTransition open = new TranslateTransition(new Duration(350), this.gameOverPane);
         open.setToY(0);
         open.play();
-        this.restartBtn.setDefaultButton(true);
+        open.setOnFinished(event -> this.restartBtn.setDefaultButton(true));
     }
 
     public void hide() {
+        this.restartBtn.setDisable(true);
         this.restartBtn.setDefaultButton(false);
         TranslateTransition close = new TranslateTransition(new Duration(350), this.gameOverPane);
         close.setToY(350);
